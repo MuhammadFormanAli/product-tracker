2251b908a7021a1fe6e6de209468b8267d67c67e
MuhammadFormanAli <muhammadformaanali@gmail.com>
2026-01-01 16:55:08 +0600

    updated:add-product

--- app/add-product/page.jsx
+++ app/add-product/page.jsx
@@ -14,9 +14,13 @@ const AddProduct = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
   // const [submittedData, setSubmittedData] = useState("");
+
+  const status = watch("status");
+  //   console.log(status);
 
   const onSubmit = async (data) => {
     // setSubmittedData(JSON.stringify(data, null, 2));
@@ -48,174 +52,258 @@ const AddProduct = () => {
     <div className=" bg-gray-100">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="bg-white p-8 rounded-lg shadow-md w-full grid grid-cols-2 gap-[10px] "
+        className="bg-white p-8 rounded-lg shadow-md w-full flex flex-col gap-[20px] "
       >
-        {/*1. Category field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="category">
-            Category
-          </label>
+        <div className="grid grid-cols-2 gap-[10px]">
+          {/*1. Category field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="category">
+              Category
+            </label>
 
-          <select
-            {...register("category")}
-            type="text"
-            id="category"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.category ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          >
-            <option value="">Select Category</option>
-            {categories.map((item) => (
-              <option key={item.value} value={item.value}>
-                {item.name}
-              </option>
-            ))}
-          </select>
+            <select
+              {...register("category")}
+              type="text"
+              id="category"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.category ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            >
+              <option value="">Select Category</option>
+              {categories.map((item) => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
 
-          {/* {errors.name && (
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.category.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/*2. serialNumber field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="serialNumber">
-            Serial Number
-          </label>
-          <input
-            {...register("serialNumber", {
-              required: "Serial Number is required",
-            })}
-            type="text"
-            id="serialNumber"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.serialNumber ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/*2. serialNumber field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="serialNumber">
+              Serial Number
+            </label>
+            <input
+              {...register("serialNumber", {
+                required: "Serial Number is required",
+              })}
+              type="text"
+              id="serialNumber"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.serialNumber ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.serialNumber.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/*3 brand field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="brand">
-            Brand
-          </label>
-          <input
-            {...register("brand")}
-            type="text"
-            id="brand"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.brand ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/*3 brand field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="brand">
+              Brand
+            </label>
+            <input
+              {...register("brand")}
+              type="text"
+              id="brand"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.brand ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.brand.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/* 4. model field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="model">
-            Model
-          </label>
-          <input
-            {...register("model")}
-            type="text"
-            id="model"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.model ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/* 4. model field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="model">
+              Model
+            </label>
+            <input
+              {...register("model")}
+              type="text"
+              id="model"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.model ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.model.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/* 5.status field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="status">
-            Status
-          </label>
-          <select {...register("status")}
-            type="text" id="status" placeholder="" className=
-            {`w-full px-4 py-2 border ${
-              errors.status ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}>
-            
-            <option value="">Select Status</option>
-            <option value="inStock">In Stock</option>
-            <option value="inUse">In Use</option>
-            <option value="inRepair">In Repair</option>
-            <option value="damage">Damage</option>
-          </select>
-
-          {/* {errors.name && (
-            <p className="text-red-500 text-sm mt-1">{errors.status.message}</p>
-          )} */}
-        </div>
-
-
-        {/* 6. purchaseDate field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="purchaseDate">
-            Purchase Date
-          </label>
-          <input
-            {...register("purchaseDate")}
-            type="date"
-            id="purchaseDate"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.purchaseDate ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/* 5. purchaseDate field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="purchaseDate">
+              Purchase Date
+            </label>
+            <input
+              {...register("purchaseDate")}
+              type="date"
+              id="purchaseDate"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.purchaseDate ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.purchaseDate.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/* 7. warranty field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="warranty">
-            Warranty Period
-          </label>
-          <input
-            {...register("warranty")}
-            type="text"
-            id="warranty"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.warranty ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/* 6. warranty field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="warranty">
+              Warranty Period
+            </label>
+            <input
+              {...register("warranty")}
+              type="text"
+              id="warranty"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.warranty ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.warranty.message}</p>
           )} */}
-        </div>
+          </div>
 
-        {/* 8. remarks field  */}
-        <div>
-          <label className="block text-gray-700 mb-1" htmlFor="remarks">
-            Remarks
-          </label>
-          <input
-            {...register("remarks")}
-            type="text"
-            id="remarks"
-            placeholder=""
-            className={`w-full px-4 py-2 border ${
-              errors.remarks ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
-          />
-          {/* {errors.name && (
+          {/* 7. remarks field  */}
+          <div>
+            <label className="block text-gray-700 mb-1" htmlFor="remarks">
+              Remarks
+            </label>
+            <input
+              {...register("remarks")}
+              type="text"
+              id="remarks"
+              placeholder=""
+              className={`w-full px-4 py-2 border ${
+                errors.remarks ? "border-red-500" : "border-gray-300"
+              } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+            {/* {errors.name && (
             <p className="text-red-500 text-sm mt-1">{errors.remarks.message}</p>
           )} */}
+          </div>
+        </div>
+
+        {/* 8.status field  */}
+
+        {/* Status radio fields */}
+        <div className="flex gap-[20px]">
+          <div className="flex gap-[10px] items-center justify-center">
+            <label htmlFor="inUse">In Use</label>
+            <input
+              {...register("status")}
+              type="radio"
+              id="inUse"
+              value="inUse"
+            />
+          </div>
+
+          <div className="flex gap-[10px] items-center justify-center">
+            <label htmlFor="inRepair">In Repair</label>
+            <input
+              {...register("status")}
+              type="radio"
+              id="inRepair"
+              value="inRepair"
+            />
+          </div>
+
+          <div className="flex gap-[10px] items-center justify-center">
+            <label htmlFor="damage">Damage</label>
+            <input
+              {...register("status")}
+              type="radio"
+              id="damage"
+              value="damage"
+            />
+          </div>
+        </div>
+
+
+
+
+
+{/* In Use Form Section */}
+        <div
+          className={`overflow-hidden transition-all duration-1000
+    ${status === "inUse" ? "max-h-40 opacity-100" : "max-h-0 opacity-0"}
+  `}
+        >
+          <div className="border rounded p-2">
+            {/* 1. User Name field  */}
+            <div>
+              <label
+                className="block text-gray-700 mb-1"
+                htmlFor="userName"
+              >
+                User Name
+              </label>
+              <input
+                {...register("userName", {
+                  required: " Name is required",
+                })}
+                type="text"
+                id="userName"
+                placeholder=""
+                className={`w-full px-4 py-2 border ${
+                  errors.userName ? "border-red-500" : "border-gray-300"
+                } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              />
+              {/* {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.userName.message}</p>
+          )} */}
+            </div>
+          </div>
+        </div>
+
+
+{/* In Repair Form Section */}
+        <div
+          className={`overflow-hidden transition-all duration-1000
+    ${status === "inRepair" ? "max-h-40 opacity-100" : "max-h-0 opacity-0"}
+  `}
+        >
+          <div className="border rounded p-2">
+            {/* 1. Service Center field  */}
+            <div>
+              <label
+                className="block text-gray-700 mb-1"
+                htmlFor="serviceCenter"
+              >
+                Service Center Name
+              </label>
+              <input
+                {...register("serviceCenter", {
+                  required: "Service Center Name is required",
+                })}
+                type="text"
+                id="serviceCenter"
+                placeholder=""
+                className={`w-full px-4 py-2 border ${
+                  errors.serviceCenter ? "border-red-500" : "border-gray-300"
+                } rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              />
+              {/* {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.serviceCenter.message}</p>
+          )} */}
+            </div>
+          </div>
         </div>
 
         <input

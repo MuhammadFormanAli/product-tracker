97d9695a3e1fd77c0b60209ba98938e821d2c3f1
MuhammadFormanAli <muhammadformaanali@gmail.com>
2026-02-06 20:23:22 +0600

    adfffh

--- hooks/useProducts.js
+++ hooks/useProducts.js
@@ -14,19 +14,3 @@ export const useProducts = ({ page, search, status }) => {
     keepPreviousData: true,
   });
 };
-
-// import { useQuery } from "@tanstack/react-query";
-// import axios from "axios";
-
-// export const useProducts = ({ page, search, status }) => {
-//   return useQuery({
-//     queryKey: ["products", page, search, status],
-//     queryFn: async () => {
-//       const res = await axios.get("/api/products", {
-//         params: { page, search, status },
-//       });
-//       return res.data;
-//     },
-//     keepPreviousData: true,
-//   });
-// };
